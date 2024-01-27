3f63aa6355713987c494d06ee4ee2be3edd31e87
Hiroaki Kawata <maemaewater@gmail.com>
2024-01-27 11:48:59 +0900

    add files

---
+++ scripts/web_voice_command.js
@@ -0,0 +1,42 @@
+// verison: 0.1
+// wirtten by hiroakiwater
+class WebVoiceCommand {
+  constructor(commands, words) {
+    this.commands = commands;
+    this.words = words;
+
+    this.SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+  }
+
+  startListen() {
+    this.recognition = new this.SpeechRecognition();
+    this.recognition.continuous = true;
+    this.recognition.lang = 'en-US';
+    this.recognition.onresult = (event) => {
+      const length = event.results.length - 1;
+      const results = event.results;
+      const text = results[length][0].transcript;
+      // console.log(text);
+
+      const aTags = document.getElementsByTagName('a');
+      for (const command of commands) {
+        if (text.toLowerCase().indexOf(command.name) >= 0) {
+          for (const word of this.words) {
+            for (const link of aTags) {
+              if (link.innerText.toLowerCase().indexOf(word) >= 0) {
+                // console.log(link);
+                const event = new MouseEvent('click', {view: window, bubbles: true, cancelable: true});
+                link.dispatchEvent(event);
+              }
+            }
+          }
+        }
+      }
+    };
+    this.recognition.start();
+  }
+
+  stopListen() {
+    this.recognition.stop();
+  }
+}
